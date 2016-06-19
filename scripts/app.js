259d2f20d24656a77b6723b249ccf287727183fa
David Rubio <drbotro@hotmail.com>
2016-06-19 20:22:30 +0200

    state inicial en el component Card

--- scripts/app.js
+++ scripts/app.js
@@ -53,14 +53,27 @@ var CheckList = React.createClass({
 });
 
 var Card = React.createClass({
+    getInitialState: function(){
+        return { showDetails: false }
+    },
     render(){
-        return (
-            <div className="card">
-                <div className="card__title">{this.props.title}</div>
+        let cardDetails;
+        if (this.state.showDetails){
+            cardDetails =  (
                 <div className="card__details">
                     {this.props.description}
                     <CheckList cardId={this.props.id} tasks={this.props.tasks} />
                 </div>
+            );
+        };
+        return (
+            <div className="card">
+                <div className="card__title" onClick={
+                    ()=>this.setState({showDetails: !this.showDetails})
+                }>
+                    {this.props.title}
+                </div>
+                {cardDetails}
             </div>
         );
     }
